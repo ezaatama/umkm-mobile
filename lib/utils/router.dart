82358800699e9d043d383e729123805e69e95d4b
ezaatama <ezatama33@gmail.com>
2025-01-17 15:34:28 +0700

    update router

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:umkm/view/onboarding/splash_screen.dart';
+import 'package:umkm/view/registration/identitas_diri_screen.dart';
 import 'package:umkm/view/registration/informasi_lain_screen.dart';
+import 'package:umkm/view/registration/jenis_usaha_screen.dart';
 import 'package:umkm/view/registration/pekerjaan_finansial_screen.dart';
 import 'package:umkm/view/registration/registration_screen.dart';
 
@@ -11,11 +13,15 @@ class AppRoute {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case '/registration':
         return MaterialPageRoute(builder: (_) => const RegistrationScreen());
+      case '/identitas-diri':
+        return MaterialPageRoute(builder: (_) => const IdentitasDiriScreen());
       case '/informasi-lain':
         return MaterialPageRoute(builder: (_) => const InformasiLainScreen());
       case '/pekerjaan-finansial':
         return MaterialPageRoute(
             builder: (_) => const PekerjaanFinansialScreen());
+      case '/jenis-usaha':
+        return MaterialPageRoute(builder: (_) => const JenisUsahaScreen());
       default:
         return _routeError();
     }
